602d86c412a6007e139e25dc5cb3489cb0b78481
oways-work <owayswork@gmail.com>
2025-11-02 03:27:32 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -24,6 +24,9 @@ void print_list(const listint_t *list);
 /* --- Project Task Functions --- */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 
 #endif /* SORT_H */
+
